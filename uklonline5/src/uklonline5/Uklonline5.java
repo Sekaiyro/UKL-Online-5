7e08ac86bb70e0d1bffaa805e54e80838665cfc5
Adrian Pramana Wijaya <106787436+Adrian-Pramana-Wijaya@users.noreply.github.com>
2022-06-15 00:11:02 +0700

    Add files via upload

---
+++ uklonline5/src/uklonline5/Uklonline5.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uklonline5;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Uklonline5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int a[][]={{2,5},{3,3}};
+    int b[][]={{1,3},{5,4}};
+    System.out.println("Hasil A-B");
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            System.out.println(Arrays.toString(a));
+        }
+    }
+            
+    }
+    
+}
